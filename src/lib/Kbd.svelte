d44656c5a6e1ad574c514e242ae65d0753791bf1
ClayenKitten <dima070704@yandex.ru>
2023-10-01 16:49:44 +0300

    Add `Kbd` component to display shortcuts

---
+++ src/lib/Kbd.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    export let ctrl: boolean = false;
+    export let shift: boolean = false;
+    export let key: string;
+
+    $: data =
+        (ctrl ? "<kbd>Ctrl</kbd> + " : "") +
+        (shift ? "<kbd>Shift</kbd> + " : "") +
+        `<kbd>${key}</kbd>`;
+</script>
+
+<kbd>{@html data}</kbd>
+
+<style lang="scss">
+    kbd,
+    kbd > :global(kbd) {
+        font-size: 14px;
+        word-spacing: -0.25em;
+    }
+</style>
