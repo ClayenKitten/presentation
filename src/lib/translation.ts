3946e7aeb9f9fc0621b863e536552c830085a527
ClayenKitten <dima070704@yandex.ru>
2023-10-01 19:56:06 +0300

    Refactor i18n

--- src/lib/translation.ts
+++ src/lib/translation.ts
@@ -1,93 +1,165 @@
-const translations: Record<string, Record<string, string>> = {
-    en: {
-        "slideshow": "Slideshow",
-        "access": "Access",
+import { get, writable, type Writable } from "svelte/store";
 
-        "file": "File",
-        "edit": "Edit",
-        "insert": "Insert",
-        "slide": "Slide",
+type Language = "en" | "ru";
 
-        "action/new": "New",
-        "action/open": "Open",
-        "action/download": "Download",
-        "action/new_slide": "New slide",
-        "action/duplicate_slide": "Duplicate slide",
-        "action/delete_slide": "Delete slide",
-        "action/background": "Background",
-        "action/transition": "Transition",
+/** Type that ensures translation to all languages */
+type Translations = {
+    en: string,
+    ru: string,
+}
 
-        "action/undo": "Undo",
-        "action/redo": "Redo",
-        "action/cut": "Cut",
-        "action/copy": "Copy",
-        "action/paste": "Paste",
-        "action/delete": "Delete",
+export const language: Writable<Language> = writable("ru");
 
-        "action/insert/text": "Textbox",
-        "action/insert/image": "Image",
-        "action/insert/video": "Video",
-        "action/insert/audio": "Audio",
-        "action/insert/shapes": "Shape",
-        "action/insert/qr-code": "QR-Code",
+export default function i18n(key: string): string {
+    return translations[key][get(language)];
+}
 
-        "action/insert/rect": "Rectanle",
-        "action/insert/oval": "Oval",
-        "action/insert/diamond": "Diamond",
-        "action/insert/parallelogram": "Parallelogram",
-        "action/insert/triangle": "Triangle",
-        "action/insert/connection": "Connection",
-        "action/insert/connection/line": "Line",
-        "action/insert/connection/arrow_simple": "Arrow",
-        "action/insert/connection/arrow_fat": "Fat Arrow",
+const translations: Record<string, Translations> = {
+    "slideshow": {
+        en: "Slideshow",
+        ru: "Слайд-шоу",
     },
-    ru: {
-        "slideshow": "Слайд-шоу",
-        "access": "Доступ",
+    "access": {
+        en: "Access",
+        ru: "Доступ",
+    },
 
-        "file": "Файл",
-        "edit": "Правка",
-        "insert": "Вставить",
-        "slide": "Слайд",
-
-        "action/undo": "Назад",
-        "action/redo": "Вперёд",
-        "action/cut": "Вырезать",
-        "action/copy": "Копировать",
-        "action/paste": "Вставить",
-        "action/delete": "Удалить",
-
-        "action/new": "Создать",
-        "action/open": "Открыть",
-        "action/download": "Скачать",
-        "action/new_slide": "Новый слайд",
-        "action/duplicate_slide": "Дублировать слайд",
-        "action/delete_slide": "Удалить слайд",
-        "action/background": "Задний фон",
-        "action/transition": "Переход",
-
-        "action/insert/text": "Текст",
-        "action/insert/image": "Изображение",
-        "action/insert/video": "Видео",
-        "action/insert/audio": "Аудио",
-        "action/insert/shapes": "Фигура",
-        "action/insert/qr-code": "QR-Код",
-
-        "action/insert/rect": "Прямоугольник",
-        "action/insert/oval": "Овал",
-        "action/insert/diamond": "Ромб",
-        "action/insert/parallelogram": "Параллелограм",
-        "action/insert/triangle": "Треугольник",
-        "action/insert/connection": "Соединение",
-        "action/insert/connection/line": "Линия",
-        "action/insert/connection/arrow_simple": "Стрелка",
-        "action/insert/connection/arrow_fat": "Толстая стрелка",
-    }
-}
-
-export default function i18n(key: string, lang?: string): string {
-    if (!lang) {
-        lang = "en";
-    }
-    return translations[lang][key];
-}
+    // Menu
+    "file": {
+        en: "File",
+        ru: "Файл",
+    },
+    "edit": {
+        en: "Edit",
+        ru: "Правка",
+    },
+    "insert": {
+        en: "Insert",
+        ru: "Вставка",
+    },
+    "slide": {
+        en: "Slide",
+        ru: "Слайд",
+    },
+    // Menu > File
+    "action/new": {
+        en: "New",
+        ru: "Создать",
+    },
+    "action/open": {
+        en: "Open",
+        ru: "Открыть",
+    },
+    "action/download": {
+        en: "Download",
+        ru: "Скачать",
+    },
+    // Menu > Slide
+    "action/new_slide": {
+        en: "New slide",
+        ru: "Новый слайд",
+    },
+    "action/duplicate_slide": {
+        en: "Duplicate slide",
+        ru: "Дублировать слайд",
+    },
+    "action/delete_slide": {
+        en: "Delete slide",
+        ru: "Удалить слайд",
+    },
+    "action/background": {
+        en: "Background",
+        ru: "Задний фон",
+    },
+    "action/transition": {
+        en: "Transition",
+        ru: "Переход",
+    },
+    // Menu > Edit
+    "action/undo": {
+        en: "Undo",
+        ru: "Назад",
+    },
+    "action/redo": {
+        en: "Redo",
+        ru: "Вперёд",
+    },
+    "action/cut": {
+        en: "Cut",
+        ru: "Вырезать",
+    },
+    "action/copy": {
+        en: "Copy",
+        ru: "Копировать",
+    },
+    "action/paste": {
+        en: "Paste",
+        ru: "Вставить",
+    },
+    "action/delete": {
+        en: "Delete",
+        ru: "Удалить",
+    },
+    // Menu > Insert
+    "action/insert/text": {
+        en: "Text box",
+        ru: "Текст",
+    },
+    "action/insert/image": {
+        en: "Image",
+        ru: "Изображение",
+    },
+    "action/insert/video": {
+        en: "Video",
+        ru: "Видео",
+    },
+    "action/insert/audio": {
+        en: "Audio",
+        ru: "Аудио",
+    },
+    "action/insert/shapes": {
+        en: "Shape",
+        ru: "Фигура",
+    },
+    "action/insert/qr-code": {
+        en: "QR-Code",
+        ru: "QR-код",
+    },
+    // Menu > Insert> Shape
+    "action/insert/rect": {
+        en: "Rectanle",
+        ru: "Прямоугольник",
+    },
+    "action/insert/oval": {
+        en: "Oval",
+        ru: "Овал",
+    },
+    "action/insert/diamond": {
+        en: "Diamond",
+        ru: "Ромб",
+    },
+    "action/insert/parallelogram": {
+        en: "Parallelogram",
+        ru: "Параллелограм",
+    },
+    "action/insert/triangle": {
+        en: "Triangle",
+        ru: "Треугольник",
+    },
+    "action/insert/connection": {
+        en: "Connection",
+        ru: "Соединение",
+    },
+    "action/insert/connection/line": {
+        en: "Line",
+        ru: "Линия",
+    },
+    "action/insert/connection/arrow_simple": {
+        en: "Arrow",
+        ru: "Стрелка",
+    },
+    "action/insert/connection/arrow_fat": {
+        en: "Fat Arrow",
+        ru: "Толстая стрелка",
+    },
+};
